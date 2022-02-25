e03ffcc935733758550a2d203276e75561657ac5
DreamOneX <1538874738@qq.com>
2022-02-25 21:49:25 +0800

    把许多行变成了三行

--- api.py
+++ api.py
@@ -1,9 +1,3 @@
 import urllib.request
 from bs4 import BeautifulSoup
-website = input("请输入要获取的网站IP：")
-url = r'https://ipaddress.com/website/' + website
-res = urllib.request.urlopen(url)
-html = res.read().decode('utf-8')
-soup = BeautifulSoup(html, 'html.parser')
-ip = soup.li.text
-print("IP：" + ip)
+print("IP: " + BeautifulSoup(urllib.request.urlopen(r'https://ipaddress.com/website/' + input("请输入要获取的网站IP：")).read().decode('utf-8'), 'html.parser').li.text)
